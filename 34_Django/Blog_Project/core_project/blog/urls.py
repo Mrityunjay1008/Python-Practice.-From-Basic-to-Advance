db077368008c31ff91859ff262bab79741e62d07
Mrityunjay1008 <sharmagopal1306@gmail.com>
2026-06-07 15:59:14 +0530

    Applications and Routes

---
+++ 34_Django/Blog_Project/core_project/blog/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path("", views.home, name="blog_home"),
+    path("about/", views.about, name="blog_about"),
+]
