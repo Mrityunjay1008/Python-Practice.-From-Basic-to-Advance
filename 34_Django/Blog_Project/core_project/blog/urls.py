8f36f7bdc126dc3c0ea5a63405540817e3d59c29
Mrityunjay1008 <sharmagopal1306@gmail.com>
2026-06-09 10:19:25 +0530

    Added login required Decorator and Profile Page

--- 34_Django/Blog_Project/core_project/blog/urls.py
+++ 34_Django/Blog_Project/core_project/blog/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 urlpatterns = [
     path("", views.home, name="blog_home"),
     path("about/", views.about, name="blog_about"),
+    path("profile/", views.profile, name="blog_profile"),
 ]
--- 34_Django/Blog_Project/core_project/blog/views.py
+++ 34_Django/Blog_Project/core_project/blog/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from .models import Post
 
-# Create your views here.
+@login_required(login_url="user_login")
 def home(request):
     context = {
         "posts": Post.objects.all()
     }
     return render(request,"blog/home.html",context)
 
+@login_required(login_url="user_login")
 def about(request):
     return render(request,"blog/about.html")
+
+
+@login_required(login_url="user_login")
+def profile(request):
+
+    return render(request,"blog/profile.html")
